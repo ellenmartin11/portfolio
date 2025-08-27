eb52ac2bd84b2654bacfe0b9817d8facb240f0a8
Ellen Martin <68326791+ellenmartin11@users.noreply.github.com>
2025-08-27 16:22:57 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ellenmartin11.github.io",
+  base: "/devportfolio", // This is the name of your repository
   vite: {
     plugins: [tailwindcss()],
   },
